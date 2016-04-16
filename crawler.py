50671d8cadd028eb765c80b6774e2592230bb9df
adampie <adam@pietrzycki.com>
2016-04-16 17:35:07 +0100

    Added checking of robots.txt

--- crawler.py
+++ crawler.py
@@ -1,14 +1,28 @@
 from envparse import env
-from bs4 import BeautifulSoup
-from urllib import parse, request
+from bs4 import BeautifulSoup as soup
+from urllib import parse, request, robotparser
 import MySQLdb
 
+# Connect to db
 env.read_envfile()
+MySQLdb.connect(env.str("db_ip"), env.str("db_user"), env.str("db_pass"), env.str("db_schema"))
 
-target = "https://news.ycombinator.com/"
+# Set target URL and root domain
+target = "https://news.ycombinator.com//"
+root_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=request.urlparse(target))
+
+# Check robots.txt
+robots = robotparser.RobotFileParser()
+robots_url = parse.urljoin(root_domain, "robots.txt")
+robots.set_url(robots_url)
+robots.read()
 
 site = request.urlopen(target).read()
-soup = BeautifulSoup(site, 'html.parser')
+soup = soup(site, 'html.parser')
 
 for tag in soup.findAll('a', href=True):
-    print(parse.urljoin(target, tag['href']))
+    if robots.can_fetch('*', parse.urljoin(target, tag['href'])):
+        print("ALLOWED - "+parse.urljoin(target, tag['href']))
+    else:
+        print("DISALLOWED - "+parse.urljoin(target, tag['href']))
+
