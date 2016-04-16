56b414ba1928d6bf17471598453fae4b7bb6f344
adampie <adam@pietrzycki.com>
2016-04-16 19:49:21 +0100

    Integrated MySQL

--- crawler.py
+++ crawler.py
@@ -5,10 +5,13 @@ import MySQLdb
 
 # Connect to db
 env.read_envfile()
-MySQLdb.connect(env.str("db_ip"), env.str("db_user"), env.str("db_pass"), env.str("db_schema"))
+db = MySQLdb.connect(env.str("db_ip"), env.str("db_user"), env.str("db_pass"), env.str("db_schema"))
+connect = db.cursor()
 
 # Set target URL and root domain
-target = "https://news.ycombinator.com//"
+connect.execute("SELECT address FROM crawled_urls WHERE parsed=0 ORDER BY id ASC LIMIT 1;")
+results = connect.fetchone()
+target = results[0]
 root_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=request.urlparse(target))
 
 # Check robots.txt
@@ -22,7 +25,8 @@ soup = soup(site, 'html.parser')
 
 for tag in soup.findAll('a', href=True):
     if robots.can_fetch('*', parse.urljoin(target, tag['href'])):
-        print("ALLOWED - "+parse.urljoin(target, tag['href']))
+        print("ALLOWED - " + parse.urljoin(target, tag['href']))
     else:
-        print("DISALLOWED - "+parse.urljoin(target, tag['href']))
+        print("DISALLOWED - " + parse.urljoin(target, tag['href']))
+
 
