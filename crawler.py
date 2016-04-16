56e619d55e1e112576e5153f801b67b29d4ed32e
Adam Pietrzycki <adam@pietrzycki.com>
2016-04-16 13:13:06 +0100

    Added req pip and base working crawler

--- crawler.py
+++ crawler.py
@@ -1,13 +1,15 @@
 from envparse import env
 from bs4 import BeautifulSoup
-import urllib.request
+import urllib
+import urlparse
+import MySQLdb
 
 env.read_envfile()
 
 target = "https://news.ycombinator.com/"
 
-site = urllib.request.urlopen(target).read()
+site = urllib.urlopen(target).read()
 soup = BeautifulSoup(site, 'html.parser')
 
 for tag in soup.findAll('a', href=True):
-    print(urllib.request.urljoin(target, tag['href']))
+    print(urlparse.urljoin(target, tag['href']))
