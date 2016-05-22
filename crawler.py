c87128ec57adfe61c96b6aec62cea6a158ea3b90
adampie <adam@pietrzycki.com>
2016-05-22 15:57:32 +0100

    Working crawler

--- crawler.py
+++ crawler.py
@@ -3,30 +3,55 @@ from bs4 import BeautifulSoup as soup
 from urllib import parse, request, robotparser
 import MySQLdb
 
-# Connect to db
-env.read_envfile()
-db = MySQLdb.connect(env.str("db_ip"), env.str("db_user"), env.str("db_pass"), env.str("db_schema"))
-connect = db.cursor()
+def crawlUrl():
 
-# Set target URL and root domain
-connect.execute("SELECT address FROM crawled_urls WHERE parsed=0 ORDER BY id ASC LIMIT 1;")
-results = connect.fetchone()
-target = results[0]
-root_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=request.urlparse(target))
+    # Connect to MySQL DB
+    env.read_envfile()
+    db = MySQLdb.connect(env.str("db_ip"), env.str("db_user"), env.str("db_pass"), env.str("db_schema"))
+    connect = db.cursor()
 
-# Check robots.txt
-robots = robotparser.RobotFileParser()
-robots_url = parse.urljoin(root_domain, "robots.txt")
-robots.set_url(robots_url)
-robots.read()
+    # Set target URL and root domain
+    connect.execute("SELECT address FROM crawled_urls WHERE parsed=0 ORDER BY id ASC LIMIT 1;")
+    results = connect.fetchone()
+    target = results[0]
+    root_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=request.urlparse(target))
 
-site = request.urlopen(target).read()
-soup = soup(site, 'html.parser')
+    # Check robots.txt
+    try:
+        robots = robotparser.RobotFileParser()
+        robots_url = parse.urljoin(root_domain, "robots.txt")
+        robots.set_url(robots_url)
+        robots.read()
+    except:
+        print("Check robots.txt error")
 
-for tag in soup.findAll('a', href=True):
-    if robots.can_fetch('*', parse.urljoin(target, tag['href'])):
-        print("ALLOWED - " + parse.urljoin(target, tag['href']))
-    else:
-        print("DISALLOWED - " + parse.urljoin(target, tag['href']))
+    # I do like a good Soup
+    updateUrl = "UPDATE crawled_urls SET parsed = 1 WHERE address = '" + target + "';"
+    try:
+        site = request.urlopen(target).read()
+        parsed = soup(site, 'html.parser')
 
+        # Go through the URLS
+        for tag in parsed.findAll('a', href=True):
+            url = parse.urljoin(target, tag['href'])
+            existsAndCrawled = "SELECT (parsed = 1) AS a_equals_b FROM crawled_urls WHERE address = '" + url + "';"
+            insertUrl = "INSERT INTO crawled_urls(address) VALUES ('" + url + "');"
 
+            # If can fetch and site is new then insert to db
+            if robots.can_fetch('*', url):
+                if connect.execute(existsAndCrawled) == 0:
+                    connect.execute(insertUrl)
+                    print("Added - "+url)
+    except:
+        print("Soup/Parsing error")
+        connect.execute(updateUrl)
+
+    # Set URL to parsed and close connections
+    connect.execute(updateUrl)
+    db.commit()
+    connect.close()
+
+    # Back at it again
+    crawlUrl()
+
+crawlUrl()
